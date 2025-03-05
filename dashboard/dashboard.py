fa4b84e0c5b40f133c58b57c5f7730922c428495
penhele <stephenhelk@gmail.com>
2025-03-06 03:11:51 +0700

    edit notebook

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,16 +1,14 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 df = pd.read_csv('dashboard/all_df.csv')
 
 selected_column = st.selectbox("Pilih dataset", ['Semua', 'Jam', 'Hari'])
 
 if selected_column == 'Jam':
-    df = df[[col for col in df.columns if col[-4:] == "Hour"]]
+    df = df[['dteday'] + [col for col in df.columns if 'Hour' in col or col == 'hr']]
 elif selected_column == 'Hari':
-    df = df[[col for col in df.columns if col[-3:] == "Day"]]
+    df = df[['dteday'] + [col for col in df.columns if 'Day' in col and col != 'hr']]
     df = df.drop_duplicates(subset=['instantDay'])
 else:
     df = df
